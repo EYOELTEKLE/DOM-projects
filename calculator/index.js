0f2ddd68cf2ad3f256fffd4352e98ae767b253b4
EyoelTekle <Eyoeltekle10@gmail.com>
2022-09-08 16:38:05 +0300

    added RPN(parsing) and polished the code

--- calculator/index.js
+++ calculator/index.js
@@ -28,17 +28,17 @@ submit.addEventListener("click", () =>
 {
 
 	let NumberParser = /[0-9]+/g;
-	let operations = /[/*-+]+/g;
+	let operations = /[/*/\-+]+/g;
 	let stack = [];
 	let val = input.value.split('')
 	let ops = input.value.match(operations);
 	let nums = input.value.match(NumberParser);
-	if (val[0] in nums)
-	{
-		for (let i = 0; i < val.length; i++)
-		{
-
-		}
+	let postfix = [ ...nums, ...ops];
+	let ans = evalRPN(postfix);
+	if(ans === NaN) input.value="Inserst Correct Answer";
+	else{
+	inputText = "";
+	input.value = "   Answer:   " + ans;
 	}
 })
 clear.addEventListener("click", () => 
@@ -61,6 +61,53 @@ special.forEach((item) => {
 		})
 	});
 input.disabled = true;
+var evalRPN = function(tokens) {
+  
+    let nums = [];
+    let sum = 0;
+
+    for (let item of tokens)
+    {
+        if(!(isNaN(item))) nums.push(Number(item));
+        if (item == "+")
+        {
+            
+            sum = nums[nums.length - 2] + nums[nums.length - 1];
+            nums.pop();
+            nums.pop();
+            nums.push(sum)
+            
+        }
+        if (item == "-")
+        {
+            sum =    nums[nums.length - 2] - nums[nums.length - 1];
+             nums.pop();
+            nums.pop();
+            nums.push(sum)
+        }
+        if (item == "/")
+        {
+            sum = nums[nums.length - 2] / nums[nums.length - 1];
+            nums.pop();
+            nums.pop();
+            if (sum > -1 && sum <= 0 )
+            {
+                nums.push(0)
+            }
+            
+           else{ nums.push(Math.trunc(sum))}
+        }
+        if (item == "*")
+        {
+            sum = nums[nums.length - 2] * nums[nums.length - 1];
+            nums.pop();
+            nums.pop();
+            nums.push(sum)
+        }
+    }
+   
+    return nums.pop();;
+};
 /**
 remaining parsing logic
 fixed two events from firing off
